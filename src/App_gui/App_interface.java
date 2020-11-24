4c478d687bf926b190e36696c232c4a3692a76c5
Piotr Kielak <piotrek15510@gmail.com>
2020-11-24 17:38:22 +0000

    improve gui, taking main program to the app interface

--- src/App_gui/App_interface.java
+++ src/App_gui/App_interface.java
@@ -6,6 +6,16 @@
 package App_gui;
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*; 
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+//import javax.swing.filechooser.FileSystemView;
+import java.io.File;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 /**
  *
@@ -13,38 +23,85 @@ import javax.swing.*;
  */
 public class App_interface extends JFrame
 {
+    String path;
     public App_interface()
     {
  //setting layout and main panel initiation
     JPanel mainPanel = new JPanel();
     mainPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
     GroupLayout layout = new GroupLayout(mainPanel);
-    mainPanel.setPreferredSize(new Dimension(400,250));
+    mainPanel.setPreferredSize(new Dimension(750,550));
     mainPanel.setLayout(layout);
     this.getContentPane().add(mainPanel);
     
     
      
     //label initiation
-    JLabel label=new JLabel("Choose imagie with Braille language");
+    JLabel label=new JLabel("Choose imagie with Braille language:");
     mainPanel.add(label);
 
-    //tree initiation
-    JTree tree=new JTree();
-    mainPanel.add(tree);
-    tree.setSize(200,200); 
+    //file chooser initiation
+    JFileChooser fc=new JFileChooser();
+    mainPanel.add(fc);
+    fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+    fc.setAcceptAllFileFilterUsed(false);
+    FileNameExtensionFilter filter = new FileNameExtensionFilter("PNG and JPG images", "png", "jpg");
+    fc.addChoosableFileFilter(filter);
+    
+    ActionListener action2 = new ActionListener() {
+        
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            File selectedFile = fc.getSelectedFile();
+            path=selectedFile.getAbsolutePath();
+            String destinationPath = "D:/obrazek-edited.jpg";
+        
+        // Welcome message
+        System.out.println("Welcome to Braille Translator! \nPath to the chosen photo: " + path);
+            
+        // Loading the OpenCV native library
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        
+        // Preparing matrix for changing an image
+        Mat imgEdited = new Mat();
+        Mat image = Imgcodecs.imread(path, 1);  
+
+        double w,h;
+        w = image.size().width;
+        h = image.size().height;
+        System.out.println("Image size: \nWidth: " + w + "px\nHeight: " + h +"px");
+    
+        //Function call
+        image_edition(imgEdited, image);
+        
+        //Saving edited image
+        Imgcodecs.imwrite(destinationPath,imgEdited );
+        // Successful operation message
+        System.out.println("The photo was succesfully edited and saved to a new file! \nPath to the edited photo: " + destinationPath);
+        
+       
+            JOptionPane.showMessageDialog(null,"The image has been selected");
+        }};
+    fc.addActionListener(action2);
+    
     
     //buttons initiation
     JButton button1= new JButton("Translate");
     mainPanel.add(button1);
     button1.setSize(15,15); 
-    //button1.addActionListener();  
-    
+  
     JButton button2= new JButton(new ImageIcon("C:\\Users\\piotr\\Desktop\\Braille-Trans\\Braille-Project\\src\\App_gui\\info.png"));
     mainPanel.add(button2);
-    button2.setSize(10,10); 
-    //button2.addActionListener();  
     
+    ActionListener action1 = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JOptionPane.showMessageDialog(null,"Braille Translator is a console application that allows user to translate Braille alphabate to traditional English alphabet. \n" +
+        "  User needs to prepare a sharp image of a text written in Braille and to provide information about a path to the file. \n" +
+        "  Then using OpenCV 4.5.0 app edits the image and using special algorithm translates text to finally save it to a new file.","information",JOptionPane.INFORMATION_MESSAGE);
+        }};
+
+    button2.addActionListener(action1);
     //Check box initiation
     JCheckBox checkBox=new JCheckBox("save as .txt");  
     mainPanel.add(checkBox);
@@ -59,7 +116,7 @@ public class App_interface extends JFrame
     GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
 
    hGroup.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING).
-            addComponent(label).addComponent(tree).addComponent(checkBox));
+            addComponent(label).addComponent(fc).addComponent(checkBox));
    hGroup.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING).
             addComponent(button2).addComponent(button1));
    layout.setHorizontalGroup(hGroup);
@@ -68,46 +125,30 @@ public class App_interface extends JFrame
    GroupLayout.SequentialGroup vGroup = layout.createSequentialGroup();
    vGroup.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE).
             addComponent(label).addComponent(button2));
-   vGroup.addGroup(layout.createParallelGroup().addComponent(tree));
+   vGroup.addGroup(layout.createParallelGroup().addComponent(fc));
    vGroup.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE).
             addComponent(checkBox).addComponent(button1));
    layout.setVerticalGroup(vGroup);
-}
-
-    public static void main(String[] args)
-    {
-        Runnable thread=new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                App_interface app = new App_interface();
-                app.setVisible(true);
-            }
-        };
-        SwingUtilities.invokeLater(thread);
     }
-}
+   public String getpath() 
+   {
+   return path;
+   }
+   static void image_edition(Mat img_Gray, Mat image) {
+            System.out.println("The edition has started! I'm currently editing the image...");
+            //Grayscale
+            Imgproc.cvtColor(image, img_Gray, Imgproc.COLOR_BGR2GRAY);
 
-class information extends JDialog
-{
-    public information()
-    {
-        information infoPanel=new information();
-        infoPanel.setPreferredSize(new Dimension(400,350));
-        this.getContentPane().add(infoPanel);
-        JTextArea info_text=new JTextArea("Braille Translator is a console application that allows user to translate Braille alphabate to traditional English alphabet. \n" +
-        "  User needs to prepare a sharp image of a text written in Braille and to provide information about a path to the file. \n" +
-        "  Then using OpenCV 4.5.0 app edits the image and using special algorithm translates text to finally save it to a new file ");  
-        infoPanel.add(info_text);
-        
-        this.setTitle("Info");
-        this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-        this.setSize(300,200);
-        
-    }
-    public void runs(){
-        information dialog=new information();
-        dialog.setVisible(true);
+            //Gaussian Filter
+            Imgproc.GaussianBlur(img_Gray, img_Gray, new Size(3,3), 0);
+            Imgproc.adaptiveThreshold(img_Gray, img_Gray, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY_INV, 5, 5);
+            // Median Filter
+            Imgproc.medianBlur(img_Gray, img_Gray, 3);
+            Imgproc.threshold(img_Gray, img_Gray, 0, 255, Imgproc.THRESH_OTSU); 
+            //Gaussian Filter
+            Imgproc.GaussianBlur(img_Gray, img_Gray, new Size(3, 3), 0);
+            Imgproc.threshold(img_Gray, img_Gray, 0, 255, Imgproc.THRESH_OTSU);
+            //imagie edition message
+            System.out.println("The editing is complete!");
         }
 }
--- src/brailleMain.java
+++ src/brailleMain.java
@@ -12,9 +12,8 @@ import org.opencv.core.Core;
 import java.io.IOException; 
 
 import App_gui.App_interface;
-//import App_gui.information;
 import javax.swing.SwingUtilities;
-
+import java.awt.event.*; 
 
 /**
  * Braille Translator is a console application that allows user to translate Braille alphabate to traditional English alphabet. 
@@ -30,63 +29,67 @@ public class brailleMain {
      * @param args the command line arguments
      */  
 
-    public static void main(String[] args)throws IOException{
-        String sourcePath = "D:/obrazek.jpg";
-        String destinationPath = "D:/obrazek-edited.jpg";
-        
-        // Welcome message
-        System.out.println("Welcome to Braille Translator! \nPath to the chosen photo: " + sourcePath);
-            
-        // Loading the OpenCV native library
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        
-        // Preparing matrix for changing an image
-        Mat imgEdited = new Mat();
-        Mat image = Imgcodecs.imread(sourcePath, 1);  
-
-        double w,h;
-        w = image.size().width;
-        h = image.size().height;
-        System.out.println("Image size: \nWidth: " + w + "px\nHeight: " + h +"px");
-    
-        //Function call
-        image_edition(imgEdited, image);
-        
-        //Saving edited image
-        Imgcodecs.imwrite(destinationPath,imgEdited );
-        // Successful operation message
-        System.out.println("The photo was succesfully edited and saved to a new file! \nPath to the edited photo: " + destinationPath);
-        
+    public static void main(String[] args)throws IOException{ 
         //initiating interface
-        Runnable thread=new Runnable()
-        {
-            @Override
-            public void run()
-            {
+       Runnable thread=new Runnable()
+       {
+           @Override
+           public void run()
+           {
                 App_interface app = new App_interface();
                 app.setVisible(true);
-            }
-        };
+           }
+       };
         SwingUtilities.invokeLater(thread);
-    }
-    
-        // METHODS
-        //image edition method
-        static void image_edition(Mat img_Gray, Mat image) {
-            System.out.println("The edition has started! I'm currently editing the image...");
-            //Grayscale
-            Imgproc.cvtColor(image, img_Gray, Imgproc.COLOR_BGR2GRAY);
-
-            //Gaussian Filter
-            Imgproc.GaussianBlur(img_Gray, img_Gray, new Size(3,3), 0);
-            Imgproc.adaptiveThreshold(img_Gray, img_Gray, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY_INV, 5, 5);
-            // Median Filter
-            Imgproc.medianBlur(img_Gray, img_Gray, 3);
-            Imgproc.threshold(img_Gray, img_Gray, 0, 255, Imgproc.THRESH_OTSU); 
-            //Gaussian Filter
-            Imgproc.GaussianBlur(img_Gray, img_Gray, new Size(3, 3), 0);
-            Imgproc.threshold(img_Gray, img_Gray, 0, 255, Imgproc.THRESH_OTSU);
-            //imagie edition message
-            System.out.println("The editing is complete!");
-        }
+        
+//        App_interface app = new App_interface();
+//        String sourcePath =app.getpath();
+//        
+//        String destinationPath = "D:/obrazek-edited.jpg";
+//        
+//        // Welcome message
+//        System.out.println("Welcome to Braille Translator! \nPath to the chosen photo: " + sourcePath);
+//            
+//        // Loading the OpenCV native library
+//        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+//        
+//        // Preparing matrix for changing an image
+//        Mat imgEdited = new Mat();
+//        Mat image = Imgcodecs.imread(sourcePath, 1);  
+//
+//        double w,h;
+//        w = image.size().width;
+//        h = image.size().height;
+//        System.out.println("Image size: \nWidth: " + w + "px\nHeight: " + h +"px");
+//    
+//        //Function call
+//        image_edition(imgEdited, image);
+//        
+//        //Saving edited image
+//        Imgcodecs.imwrite(destinationPath,imgEdited );
+//        // Successful operation message
+//        System.out.println("The photo was succesfully edited and saved to a new file! \nPath to the edited photo: " + destinationPath);
+//        
+//       
+//    }
+//    
+//        // METHODS
+//        //image edition method
+//        static void image_edition(Mat img_Gray, Mat image) {
+//            System.out.println("The edition has started! I'm currently editing the image...");
+//            //Grayscale
+//            Imgproc.cvtColor(image, img_Gray, Imgproc.COLOR_BGR2GRAY);
+//
+//            //Gaussian Filter
+//            Imgproc.GaussianBlur(img_Gray, img_Gray, new Size(3,3), 0);
+//            Imgproc.adaptiveThreshold(img_Gray, img_Gray, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY_INV, 5, 5);
+//            // Median Filter
+//            Imgproc.medianBlur(img_Gray, img_Gray, 3);
+//            Imgproc.threshold(img_Gray, img_Gray, 0, 255, Imgproc.THRESH_OTSU); 
+//            //Gaussian Filter
+//            Imgproc.GaussianBlur(img_Gray, img_Gray, new Size(3, 3), 0);
+//            Imgproc.threshold(img_Gray, img_Gray, 0, 255, Imgproc.THRESH_OTSU);
+//            //imagie edition message
+//            System.out.println("The editing is complete!");
+        }      
 }
