3cdc9bb9f6d6a941f5a470d90eb14afb48a56476
Piotr Kielak <piotrek15510@gmail.com>
2021-01-11 16:01:45 +0000

    random commit

--- src/brailleMain.java
+++ src/brailleMain.java
@@ -205,6 +205,7 @@ public class brailleMain{
                             }
                         }
                     }
+                    translate.close();
                     //KONIEC
                   
                     String currCode = String.join("", currCharacter);
